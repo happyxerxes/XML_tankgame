30d870432723d6cde90fea519eafe2f7a0757fed
Xerxes <1435234681@qq.com>
2016-12-08 16:51:43 +0000

    加入开始与结束界面

--- main.cpp
+++ main.cpp
@@ -10,55 +10,30 @@
 
 
 
-void start_game(sf::RenderWindow &window){
-    sf::Event event;
-    sf::Texture startgame_texture;
-    if (!startgame_texture.loadFromFile("/Users/XHZ/Library/Developer/Xcode/DerivedData/XML_Tank-endjjnmbmexpiehiwzrtpzwayevi/Build/Products/Release/XML_Tank.app/Contents/Resources/gamestart.jpg")) {
-        return;
-    }
-    sf::Sprite startgame_sprite(startgame_texture);
-    
-    
-    
-    window.draw(startgame_sprite);
-    window.display();
-    
-    bool flag = true;
-    
-    while (window.isOpen() && flag){
-        while (window.pollEvent(event))
-        {
-            bool close = (event.type == sf::Event::Closed);
-            //≈–∂œ «∑Ò∞¥ESC
-            bool escape = (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape);
-            if (close || escape)
-                window.close();
-            
-            
-            if (event.type == sf::Event::MouseButtonPressed)
-            {
-                if (event.mouseButton.button == sf::Mouse::Left)
-                {
-                    flag = false;
-                    break;
-                }
-            }
-        }
-        
-    }
-}
 
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "TANK");
+    Game *game = new Game();
+    game->delete_game();
     
+    while (window.isOpen()) {
+        game->start_game(window);
+        
+        
+        
+        game->play_game(window);
+        
+        game->delete_game();
+        
+        printf("1");
+        
+        
+        
+    }
     
-    start_game(window);
-    
-	Game game(window);
-    
-    
-	return 0;
+    delete game;
+    return 0;
 }
 
 
