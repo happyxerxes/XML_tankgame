52edd6e2560a6f3af8f16b5f9f63ca62d4bd2562
Xerxes <1435234681@qq.com>
2016-12-08 16:40:37 +0000

    加入buff

---
+++ buff.cpp
@@ -0,0 +1,61 @@
+#include "buff.h"
+using namespace std;
+
+Buff::Buff(int type) :RectangleShape(sf::Vector2f(BUFF_WIDTH, BUFF_HEIGHT)){
+    //int type;
+
+    sf::Vector2f buff_position;
+    buff_position.x = (rand() % (RAND_MAX_X - RAND_MIN_X)) + RAND_MIN_X;
+    buff_position.y = (rand() % (RAND_MAX_Y - RAND_MIN_Y)) + RAND_MIN_Y;
+    //type=(rand() % (TYPE_MAX - TYPE_MIN + 1)) + TYPE_MIN;
+    if (type == 1)
+    {
+        //this->buff_texture.loadFromFile("buff1.png");
+        this->setFillColor(sf::Color::Blue);
+        this->type = type;
+    }
+    if (type == 2)
+    {
+        //this->buff_texture.loadFromFile("buff2.png");
+        this->setFillColor(sf::Color::Red);
+        this->type = type;
+    }
+    this->setPosition(buff_position.x, buff_position.y);
+    this->setOrigin(BUFF_WIDTH / 2, BUFF_HEIGHT / 2);
+    
+}
+
+int Buff::tank_buff_collision(Tank *tank){
+    sf::Vector2f tank_position;
+    sf::Vector2f buff_position;
+    sf::Vector2f position_distance;
+    float distance;
+    tank_position = tank->getPosition();
+    buff_position = this->getPosition();
+    position_distance.x = tank_position.x-buff_position.x;
+    position_distance.y = tank_position.y - buff_position.y;
+    distance = (sqrt(position_distance.x*position_distance.x + position_distance.y*position_distance.y ));
+    if (distance < BUFF_TANK_DISTANCE){
+        int temp_type=this->type;
+        this->type = 0;
+        return temp_type;
+    }
+    return 0;
+}
+
+void Buff::clear_all_bullets(Bullet *bullets[], int bullets_number){
+    
+    for (int i = 0; i < bullets_number; i++){
+        if (bullets[i] != NULL) {
+            bullets[i]->is_exist = false;
+        }        
+    }
+}
+
+void Buff::add_mytank_life(int &life){
+    life++;
+}
+
+void Buff::buff_collision_handle_one(){
+    
+}
---
+++ buff.h
@@ -0,0 +1,31 @@
+#include <SFML/Graphics.hpp>
+#include"tank.h"
+#include"bullet.h"
+#define BUFF_HEIGHT 25
+#define BUFF_WIDTH 25
+
+#define BUFF_TANK_DISTANCE 25
+#define RAND_MAX_X  700
+#define RAND_MIN_X 50
+#define RAND_MAX_Y  600
+#define RAND_MIN_Y 50
+#define TYPE_MAX 2
+#define TYPE_MIN 1
+//using namespace std
+class Buff : public sf::RectangleShape
+{
+public:
+    Buff(int type);
+    int tank_buff_collision(Tank *tank);
+    void buff_collision_handle_one();
+    
+private:
+    
+    sf::Texture buff_texture;
+    bool is_exist = 0;
+public:
+    int type = 0;
+    void clear_all_bullets(Bullet *bullets[], int bullets_number);
+    void add_mytank_life(int &life);
+};
+
